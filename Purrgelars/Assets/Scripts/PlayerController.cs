62699a6d2e8b8c4aa80799cc01c4b61a9ab9f0ac
Magicorn <lemagicorn@gmail.com>
2024-10-10 19:36:56 +0200

    Remade movement script

--- Purrgelars/Assets/Scripts/PlayerController.cs
+++ Purrgelars/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public float speed = 10.0f;
-    private float movementX;
-    private float movementY;
+    Rigidbody2D rb;
 
+    // Float for the horizontal input
+    private float horizontal;
 
-    // Start is called before the first frame update
+    // Float for player movespeed
+    private float speed = 8f;
+
+    // A bool we'll be using to check which way the player is moving
+    private bool isFacingRight = true;
+
     private void Start()
     {
+        // Links the Rigidbody in the scipt to the playercharacters Rigidbody
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void OnMove(InputValue movementValue)
+    private void Update()
     {
-        // Gets the keyboard inputs and converts it to vectors?
-        Vector2 movementVector = movementValue.Get<Vector2>();
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        // Checks which way the player is facing, and applies the flip function.
+        if (!isFacingRight && horizontal > 0f)
+        {
+            Flip();
+        }
+        else if (isFacingRight && horizontal < 0f)
+        {
+            Flip();
+        }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        // Applies the movement vector values to the RigidBody
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+        // Applies the movement speed vector to the characters Rigidbody multiplied by the speed parameter.
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
 
+    private void Flip()
+    {
+        // Multiplies the x-scale of the character by -1, flipping the sprite.
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        // This tells us when an action is triggered, and returns a vector2, from which we want the x value
+        horizontal = context.ReadValue<Vector2>().x;
     }
 }
